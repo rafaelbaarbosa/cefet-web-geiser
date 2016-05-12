d5580eb780c49c541243a7a5765268f29fc61060
Rafael <rafaelbarbosamac@gmail.com>
2016-05-12 12:28:02 -0300

    Primeiro commit

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
 var express = require('express'),
     app = express();
 
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
+var fs = require('fs');
 var db = {
 };
+db.jogadores = JSON.parse(fs.readFileSync('server/data/jogadores.json', 'utf8'));
+db.jogosPorJogador = JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json', 'utf8'));
+
+// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
+// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
+// dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs)) OK!!!!!!
 
 
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+// configurar qual templating engine usar. Sugestão: hbs (handlebars) OK!!!!!!
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
+app.get('/', function(request, response){
+	response.render('index');
+});
 
 
 // EXERCÍCIO 2
@@ -27,7 +35,10 @@ var db = {
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
-
+// dica: 1 linha de código OK!!!!!!
+app.use(express.static('client/'));
 // abrir servidor na porta 3000
-// dica: 1-3 linhas de código
+// dica: 1-3 linhas de código OK!!!!!!
+var server = app.listen(3000, function () {
+  console.log('Escutando em: http://localhost:3000');
+});
